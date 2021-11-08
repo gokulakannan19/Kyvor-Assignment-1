92f47923c18ecf5aee2f0c43221e41e4491db301
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-11-08 14:57:50 +0530

    rest framework configuration completed

--- crud_application/settings.py
+++ crud_application/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework'
+
     'crud.apps.CrudConfig',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
