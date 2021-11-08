78d69d796d74e69cd83aa2af5336739d6ddacfae
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-11-08 12:57:44 +0530

    login functionality added

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.users, name="users"),
+    path('login/', views.login_user, name="login-user"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
-def users(request):
-    return HttpResponse("Testing")
+def login_user(request):
+    page = "login"
 
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print("Username or password is incorrect")
+
+    return render(request, 'users/login-register.html')
