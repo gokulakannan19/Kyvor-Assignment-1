c52e45a57d98b0310ed88f7f0df075e91c97e3b7
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-11-09 12:30:47 +0530

    genelist rendering completed

--- gene/views.py
+++ gene/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Genes, Variants
+
 
 def home(request):
-    return HttpResponse("Testing")
+    genes = Genes.objects.all()
+    context = {
+        'genes': genes
+    }
+    return render(request, "gene/gene_list.html", context)
