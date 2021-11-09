92e24d7791e6256707e4dec415f739861a858a5f
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-11-09 10:35:45 +0530

    bootstrap added

--- crud/forms.py
+++ crud/forms.py
@@ -6,3 +6,10 @@ class PatientForm(ModelForm):
     class Meta:
         model = Patient
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(PatientForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {'class': 'form-control', })
